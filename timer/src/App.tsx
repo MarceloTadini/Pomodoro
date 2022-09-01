a91b2ea4a29d13031655879d565fa9932750c0ea
Marcelo Tadini <tadini.marcelo53@gmail.com>
2022-09-01 11:59:24 -0300

    Component Header, e Home styles

--- timer/src/App.tsx
+++ timer/src/App.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <h1>Hello!</h1>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
