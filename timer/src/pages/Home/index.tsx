a0a449c5d69a684cb6a2534356b767b2f596fdb8
Marcelo Tadini <tadini.marcelo53@gmail.com>
2022-09-14 15:41:49 -0300

    Adding reset form

--- timer/src/pages/Home/index.tsx
+++ timer/src/pages/Home/index.tsx
@@ -16,18 +16,29 @@ import {
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa!'),
   minutesAmount: zod
-  .number()
-  .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
-  .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// } Automatização do processo feito através do zod.infer -> typeof utilizado pois é uma variável js
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch, formState } = useForm({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data) {
-    
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    reset()
   }
 
   const task = watch('task')
